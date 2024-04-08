1c9740f9a65d8e6fecaf00464044cf0ec1c8fb4e
Eduardo Villota <eduardouio7@gmail.com>
2024-04-08 16:34:18 -0500

    fin del problema de elimnar y agregar items

--- src/main.js
+++ src/main.js
@@ -5,8 +5,6 @@ import router from './router'
 import store from '../store'
 
 const app = createApp(App)
-
 app.use(router)
 app.use(store)
-
 app.mount('#app')
--- store/index.js
+++ store/index.js
@@ -1,39 +1,81 @@
 import { createStore } from 'vuex'
-import { qoutesData } from './data';
+import { qoutesData } from './data'
 
 const store = createStore({
-    namespaced: true,
-    state: {
-        quotes: qoutesData(),
-        supplierQoutes: [],
+  namespaced: true,
+  state: {
+    quotes: qoutesData(),
+    supplierQoutes: []
+  },
+  getters: {
+    getCountries: (state) => {
+      return state.quotes
+        .filter((quote, idx, o_array) => {
+          if (o_array.findIndex((q) => q.pais === quote.pais) === idx) {
+            return true
+          }
+        })
+        .map((q) => q.pais)
+        .sort()
     },
-    getters: {
-        getCountries: (state) => {
-            return state.quotes.filter((quote, idx, o_array) => {
-                if (o_array.findIndex((q) => q.pais === quote.pais) === idx) {
-                    return true
-                }
-            }).map((q)=>q.pais).sort();
-        },
-        getSupplier: (state) => (country) => {
-            if (country === '') return null;
-            return state.quotes.filter(
-                (quote) => quote.pais === country
-                )
-        },
-        getPortsSupplier: (state) => (supplier) => {
-            if (!supplier) return '';
-            return supplier.puertos.join(' - ');
-        },
+    getSupplier: (state) => (country) => {
+      if (country === '') return null
+      return state.quotes.filter((quote) => quote.pais === country)
     },
-    mutations: {
-        addQuote(state, payload) {
-            state.supplierQoutes.push(payload);
-        },
+    getPortsSupplier: (state) => (supplier) => {
+      if (!supplier) return ''
+      return supplier.puertos.join(' - ')
     },
-    actions: {
+    getQoutes: (state, rootStare) => {
+      if (state.supplierQoutes.length > 0) {
+        return state.supplierQoutes
+      }
+      if (localStorage.getItem('supplierQoutes')) {
+        state.supplierQoutes = JSON.parse(localStorage.getItem('supplierQoutes'))
+      }
+      return []
     },
-    modules: {
+    getQoute: (state) => (supplier) => {
+      if (state.supplierQoutes.length === 0) return false
+      const quote = state.supplierQoutes.find((quote) => quote.supplier.nombre === supplier.nombre)
+      return {
+        qoute: quote,
+        idx: state.supplierQoutes.indexOf(quote)
+      }
+    },
+    getSuppliersQouted: (state) => (nameSupplier) => {
+      if (state.supplierQoutes.length === 0) return false
+
+      const filtered = state.supplierQoutes.filter((i) => {
+        return i.supplier.nombre === nameSupplier
+      })
+      if (filtered.length) return true
+      return false
     }
-});
-export default store;
+  },
+  mutations: {
+    addQuote(state, payload) {
+      state.supplierQoutes.push(payload)
+      localStorage.clear()
+      localStorage.setItem('supplierQoutes', JSON.stringify(state.supplierQoutes))
+    },
+    deleteQoute(state, payload) {
+      state.supplierQoutes = state.supplierQoutes.filter((quote) => quote !== payload)
+      localStorage.clear()
+      localStorage.setItem('supplierQoutes', JSON.stringify(state.supplierQoutes))
+    },
+    loadSupplierQoutes: (state) => (qoutes) => {
+      state.supplierQoutes = qoutes
+    },
+    changeQoute(state, payload) {
+      localStorage.clear()
+      localStorage.setItem('supplierQoutes', JSON.stringify(state.supplierQoutes))
+    }
+  },
+  actions: {
+    sendData({ commit }, payload) {
+      const url = 'localhost:8000/'
+    }
+  }
+})
+export default store
